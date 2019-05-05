80040fd3fa4bd7b25713170e757e31cbe7945fb4
Adam Abarisyi <rifki7080@gmail.com>
2019-05-05 16:07:39 +0700

    coba

--- src/screens/home/Trending.js
+++ src/screens/home/Trending.js
@@ -10,11 +10,11 @@ class Trending extends Component {
             <ListGroup variant="flush">
               <ListGroup.Item>Cras justo odio</ListGroup.Item>
               <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-              <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+               <ListGroup.Item>Morbi leo risus</ListGroup.Item>
               <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
             </ListGroup>
           </Container>
-      </div>
+    </div>  
     );
   }
 }
