8a82d336aae48371a8fe8b2e445d96db948f2db3
Adam Abarisyi <rifki7080@gmail.com>
2019-05-05 13:22:46 +0700

    coba

---
+++ src/screens/home/Trending.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import {ListGroup,Container } from 'react-bootstrap';
+ 
+class Trending extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Trending</h2>
+          <Container>
+            <ListGroup variant="flush">
+              <ListGroup.Item>Cras justo odio</ListGroup.Item>
+              <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+              <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+              <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+            </ListGroup>
+          </Container>
+      </div>
+    );
+  }
+}
+ 
+export default Trending;
